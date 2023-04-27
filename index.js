9625816e45fbe08722551055d3557b2949bf3c34
Nikita Mikheitsev <welt992@gmail.com>
2023-04-27 14:10:26 +0200

    feat: language change

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const inputWindow = document.createElement("textarea");
-const wrapper = document.createElement("div");
-const keyboardList = [
+const keyboardList_EN = [
   {main: "`", alt: "~", row: 1, code: "Backquote"},
   {main: "1", alt: "!", row: 1, code: "Digit1"},
   {main: "2", alt: "@", row: 1, code: "Digit2"},
@@ -29,7 +28,7 @@ const keyboardList = [
   {main: "[", alt: "{", row: 2, code: "BracketLeft"},
   {main: "]", alt: "}", row: 2, code: "BracketRight"},
   {main: "\\",alt: "|", row: 2, code: "Backslash"},
-  {main: "Delete", row: 2, code: "Delete"},
+  {main: "Del", row: 2, code: "Delete"},
   {main: "CapsLock", row: 3, width: 2, code: "CapsLock"},
   {main: "a", alt: "A", row: 3, code: "KeyA"},
   {main: "s", alt: "S", row: 3, code: "KeyS"},
@@ -43,7 +42,8 @@ const keyboardList = [
   {main: ";", alt: ":", row: 3, code: "Semicolon"},
   {main: "'", alt: '"', row: 3, code: "Quote"},
   {main: "Enter", row: 3, width: 2, code: "Enter"},
-  {main: "Shift", row: 4, width: 3, code: "ShiftLeft"},
+  {main: "Shift", row: 4, width: 2, code: "ShiftLeft"},
+  {main: "En", alt: "Ru", row: 4, code: "ShiftLeft"},
   {main: "z", alt: "Z", row: 4, code: "KeyZ"},
   {main: "x", alt: "X", row: 4, code: "KeyX"},
   {main: "c", alt: "C", row: 4, code: "KeyC"},
@@ -66,9 +66,78 @@ const keyboardList = [
   {main: "\u{2193}", row: 5, code: "ArrowDown"},
   {main: "\u{2192}", row: 5, code: "ArrowRight"}
 ]
+
+const keyboardList_RU = [
+  {main: "`", alt: "~", row: 1, code: "Backquote"},
+  {main: "1", alt: "!", row: 1, code: "Digit1"},
+  {main: "2", alt: '"', row: 1, code: "Digit2"},
+  {main: "3", alt: "№", row: 1, code: "Digit3"},
+  {main: "4", alt: ";", row: 1, code: "Digit4"},
+  {main: "5", alt: "%", row: 1, code: "Digit5"},
+  {main: "6", alt: ":", row: 1, code: "Digit6"},
+  {main: "7", alt: "?", row: 1, code: "Digit7"},
+  {main: "8", alt: "*", row: 1, code: "Digit8"},
+  {main: "9", alt: "(", row: 1, code: "Digit9"},
+  {main: "0", alt: ")", row: 1, code: "Digit0"},
+  {main: "-", alt: "_", row: 1, code: "Minus"},
+  {main: "=", alt: "+", row: 1, code: "Equal"},
+  {main: "Backspace", row: 1, width: 2, code: "Backspace"},
+  {main: "Tab", row: 2, code: "Tab"},
+  {main: "й", alt: "Й", row: 2, code: "KeyQ"},
+  {main: "ц", alt: "Ц", row: 2, code: "KeyW"},
+  {main: "у", alt: "У", row: 2, code: "KeyE"},
+  {main: "к", alt: "К", row: 2, code: "KeyR"},
+  {main: "е", alt: "Е", row: 2, code: "KeyT"},
+  {main: "н", alt: "Н", row: 2, code: "KeyY"},
+  {main: "г", alt: "Г", row: 2, code: "KeyU"},
+  {main: "ш", alt: "Ш", row: 2, code: "KeyI"},
+  {main: "щ", alt: "Щ", row: 2, code: "KeyO"},
+  {main: "з", alt: "З", row: 2, code: "KeyP"},
+  {main: "х", alt: "Х", row: 2, code: "BracketLeft"},
+  {main: "ъ", alt: "Ъ", row: 2, code: "BracketRight"},
+  {main: "\\",alt: "|", row: 2, code: "Backslash"},
+  {main: "Del", row: 2, code: "Delete"},
+  {main: "CapsLock", row: 3, width: 2, code: "CapsLock"},
+  {main: "ф", alt: "Ф", row: 3, code: "KeyA"},
+  {main: "ы", alt: "Ы", row: 3, code: "KeyS"},
+  {main: "в", alt: "В", row: 3, code: "KeyD"},
+  {main: "а", alt: "А", row: 3, code: "KeyF"},
+  {main: "п", alt: "П", row: 3, code: "KeyG"},
+  {main: "р", alt: "Р", row: 3, code: "KeyH"},
+  {main: "о", alt: "О", row: 3, code: "KeyJ"},
+  {main: "л", alt: "Л", row: 3, code: "KeyK"},
+  {main: "д", alt: "Д", row: 3, code: "KeyL"},
+  {main: "ж", alt: "Ж", row: 3, code: "Semicolon"},
+  {main: "э", alt: 'Э', row: 3, code: "Quote"},
+  {main: "Enter", row: 3, width: 2, code: "Enter"},
+  {main: "Shift", row: 4, width: 2, code: "ShiftLeft"},
+  {main: "En", alt: "Ru", row: 4, code: "ShiftLeft"},
+  {main: "я", alt: "Я", row: 4, code: "KeyZ"},
+  {main: "ч", alt: "Ч", row: 4, code: "KeyX"},
+  {main: "с", alt: "С", row: 4, code: "KeyC"},
+  {main: "м", alt: "М", row: 4, code: "KeyV"},
+  {main: "и", alt: "И", row: 4, code: "KeyB"},
+  {main: "т", alt: "Т", row: 4, code: "KeyN"},
+  {main: "Ь", alt: "ь", row: 4, code: "KeyM"},
+  {main: "б", alt: "Б", row: 4, code: "Comma"},
+  {main: "ю", alt: "Ю", row: 4, code: "Period"},
+  {main: ".", alt: ",", row: 4, code: "Slash"},
+  {main: "\u{2191}", row: 4, code: "ArrowUp"},
+  {main: "Shift", row: 4, code: "ShiftRight"},
+  {main: "Control", row: 5, width: 1.5, code: "ControlLeft"},
+  {main: "Meta", row: 5, code: "MetaLeft"},
+  {main: "Alt", row: 5, code: "AltLeft"},
+  {main: " ", row: 5, width: 4, code: "Space"},
+  {main: "Alt", row: 5, code: "AltRight"},
+  {main: "Control", row: 5, width: 1.5, code: "ControlRight"},
+  {main: "\u{2190}", row: 5, code: "ArrowLeft"},
+  {main: "\u{2193}", row: 5, code: "ArrowDown"},
+  {main: "\u{2192}", row: 5, code: "ArrowRight"}
+]
 const tabSpaceLength = 4;
 var shiftMode = false;
 var tabMode = false;
+var keyBoardLayout_EN = true; // English by default
 
 function createButton(buttonObj) {
   let button = document.createElement("div");
@@ -88,36 +157,54 @@ function createButton(buttonObj) {
   button.classList.add(buttonObj.code);
   let main = document.createElement("span");
   main.textContent = buttonObj.main;
+  main.classList = "main";
   if (buttonObj.alt != undefined) {
     let sub = document.createElement("span");
     sub.textContent = buttonObj.alt;
+    sub.classList = "sub";
     button.append(sub);
   }
   button.append(main);
   return button;
 }
 
-wrapper.classList = "wrapper";
 const rowsInWrapper = 5;
-for (let i = 0; i<rowsInWrapper; i++) {
-  let row = document.createElement("div");
-  row.classList = "row";
-  wrapper.append(row);
+
+function fillWrapeper(rowsInWrapper, keyboardList) {
+  let wrapper = document.createElement("div");
+  wrapper.classList = "wrapper";
+  for (let i = 0; i<rowsInWrapper; i++) {
+    let row = document.createElement("div");
+    row.classList = "row";
+    wrapper.append(row);
+  }
+  
+  for (let i of keyboardList) {
+    wrapper.children[i.row - 1].append(createButton(i));
+  }
+  return wrapper;
 }
 
-for (let i of keyboardList) {
-  wrapper.children[i.row - 1].append(createButton(i));
+if (keyBoardLayout_EN) {
+  var wrapper = fillWrapeper(rowsInWrapper, keyboardList_EN);
+} else {
+  var wrapper = fillWrapeper(rowsInWrapper, keyboardList_RU);
 }
+
 
 document.body.append(inputWindow);
 inputWindow.focus();
 document.body.append(wrapper);
+let textDiv = document.createElement("div");
+textDiv.classList = "info";
+textDiv.textContent = "Клавиатура создана в операционной системе Windows. Для переключения языка комбинация: левыe shift + alt"
+document.body.append(textDiv);
 
 /* Listeners */
 
 /* Click listeners */
 
-wrapper.addEventListener("mousedown", (event) => {
+function mousedownHandler(event) {
   let inputWindow = document.querySelector("textarea");
   let target = event.target.closest(".button")
   if (!inputWindow.focus()) {
@@ -127,15 +214,32 @@ wrapper.addEventListener("mousedown", (event) => {
     target.classList.add("active");
     event.preventDefault();
   };
-});
+}
 
-wrapper.addEventListener("mouseup", (event) => {
+
+function mouseupHandler(event) {{
   let inputWindow = document.querySelector("textarea");
   let target = event.target.closest(".button");
   if (target) {
     let prevText = inputWindow.value.slice(0, inputWindow.selectionStart);
     let postText = inputWindow.value.slice(inputWindow.selectionEnd);
-    if (target.children.length != 1 || target.children[0].textContent == " ") { // for letters and spaces
+    if ((target.children[0].textContent == "Ru")) {
+      if (keyBoardLayout_EN) {
+        wrapper.remove();
+        wrapper = fillWrapeper(rowsInWrapper, keyboardList_RU);
+        inputWindow.after(wrapper);
+        wrapper.addEventListener("mousedown", mousedownHandler);
+        wrapper.addEventListener("mouseup", mouseupHandler);
+        keyBoardLayout_EN = !keyBoardLayout_EN;
+      } else {
+        wrapper.remove();
+        wrapper = fillWrapeper(rowsInWrapper, keyboardList_EN);
+        inputWindow.after(wrapper);
+        wrapper.addEventListener("mousedown", mousedownHandler);
+        wrapper.addEventListener("mouseup", mouseupHandler);
+        keyBoardLayout_EN = !keyBoardLayout_EN;
+      }
+    } else if (target.children.length != 1 || target.children[0].textContent == " ") { // for letters and spaces
       if (!shiftMode) { // if shift key is NOT pressed 
         if (!tabMode || !target.classList[1].includes("Key")) {
           if (target.children[1]) { // if shift key is NOT pressed AND tab key is NOT pressed
@@ -160,14 +264,14 @@ wrapper.addEventListener("mouseup", (event) => {
       inputWindow.setSelectionRange(prevText.length+1, prevText.length+1);
       target.classList.remove("active");
     } else { // for anything but letters
-      if (target.children[0].textContent == "Delete" || target.children[0].textContent == "Backspace") {
+      if (target.children[0].textContent == "Del" || target.children[0].textContent == "Backspace") {
         if (inputWindow.selectionEnd == inputWindow.selectionStart) {
           if (target.children[0].textContent == "Backspace") {
             inputWindow.value = prevText.slice(0,prevText.length-1) + postText;
             inputWindow.setSelectionRange(prevText.length-1, prevText.length-1);
             
           }
-          if (target.children[0].textContent == "Delete") {
+          if (target.children[0].textContent == "Del") {
             inputWindow.value = prevText + postText.slice(1);
             inputWindow.setSelectionRange(prevText.length, prevText.length);
           }
@@ -204,13 +308,17 @@ wrapper.addEventListener("mouseup", (event) => {
       target.classList.remove("active");
     }
   }
-});
+}
+
+}
+wrapper.addEventListener("mousedown", mousedownHandler);
+wrapper.addEventListener("mouseup", mouseupHandler);
 
 /* Key press listeners */
 
 function findFromListByKeyCode(keyCode) {
   let catchedKey = false;
-  for (let key of keyboardList) {
+  for (let key of keyboardList_EN) {
     if (key.code == keyCode) {
       catchedKey = true;
       return document.querySelector(`.${key.code}`);
@@ -221,6 +329,23 @@ function findFromListByKeyCode(keyCode) {
   }
 }
 document.addEventListener("keydown", (event) => {
+  if (event.code == "AltLeft" && event.shiftKey) {
+    if (keyBoardLayout_EN) {
+      wrapper.remove();
+      wrapper = fillWrapeper(rowsInWrapper, keyboardList_RU);
+      inputWindow.after(wrapper);
+      wrapper.addEventListener("mousedown", mousedownHandler);
+      wrapper.addEventListener("mouseup", mouseupHandler);
+      keyBoardLayout_EN = !keyBoardLayout_EN;
+    } else {
+      wrapper.remove();
+      wrapper = fillWrapeper(rowsInWrapper, keyboardList_EN);
+      inputWindow.after(wrapper);
+      wrapper.addEventListener("mousedown", mousedownHandler);
+      wrapper.addEventListener("mouseup", mouseupHandler);
+      keyBoardLayout_EN = !keyBoardLayout_EN;
+    }
+  }
   let button = findFromListByKeyCode(event.code);
   if (button) {
     button.classList.add("active");
